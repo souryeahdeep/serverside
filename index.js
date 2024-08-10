61538c8e05fedd7dcc4f6c2574ba81a623c4e0d8
souryeahdeep <165980585+souryeahdeep@users.noreply.github.com>
2024-08-11 00:17:49 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+     res.render("index.ejs", )
+});
+
+app.post("/submit", (req, res) => {
+     const numLetter=req.body["fName"].length+req.body["lName"].length;
+     console.log(numLetter);
+     res.render("index.ejs", {numberOfLetter: numLetter})
+     
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
